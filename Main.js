451c712add3ae94335a5413d80c8ced3b0c4fc63
Ron-Aran Paju <ronaran123@hotmail.com>
2022-12-03 01:50:23 +0200

    Muudatused
    Lisasin CSS ja JS ning tegin esimest alamlehte

--- Main.js
+++ Main.js
@@ -1,1 +1,8 @@
-
+function Menu() {
+    var x = document.getElementById("myTopnav"); //muutuja
+    if (x.className === "topnav") { //kui klassinimi ülemine riba
+    x.className += " responsive"; // liidab juurde ehk näitab et aktiivne ehk hoverib peal
+    } else { //vastasel juhul
+    x.className = "topnav"; // lihtsalt on seal
+    }
+}
